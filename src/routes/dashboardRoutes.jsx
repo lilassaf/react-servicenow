381f4c0f440600361aa486358c072e213adb9590
Ismail Bouzarhoun <91149489+Ismailbouzarhoun@users.noreply.github.com>
2025-04-21 17:42:50 +0100

    protected routes

--- src/routes/dashboardRoutes.jsx
+++ src/routes/dashboardRoutes.jsx
@@ -1,13 +1,18 @@
 // src/routes/dashboardRoutes.jsx
 import React from 'react';
+import PrivateRoute from '../components/PrivateRoute'; // Make sure this exists
 import DashboardLayout from '../layout/dashbord';
 import Home from '../views/dashbord/Dashboard';
 
 const dashboardRoutes = {
   path: '/dashboard',
-  element: <DashboardLayout />,
+  element: (
+    <PrivateRoute>
+      <DashboardLayout />
+    </PrivateRoute>
+  ),
   children: [
-    { index: true, element: <Home /> },        // /dashboard
+    { index: true, element: <Home /> },
     // ... other dashboard sub-routes
   ],
 };
